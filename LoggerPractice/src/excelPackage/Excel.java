f4d7e877782badc5e1716aba764bf00172949c15
tggupta <tggupta22@gmail.com>
2017-08-27 01:04:46 +0530

    First Commit eclipse

---
+++ LoggerPractice/src/excelPackage/Excel.java
@@ -0,0 +1,34 @@
+package excelPackage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Excel {
+
+	XSSFWorkbook workbook=null;
+	XSSFSheet sheet=null;
+	XSSFRow row= null;
+	XSSFCell cell=null;
+	
+	public String readExcel(int rownum, int cellnum) throws IOException
+	{
+		File file=new File("C:\\Users\\Tarun\\workspace\\LoggerPractice\\Utility\\SearchItem.xlsx");
+		 FileInputStream fis=new FileInputStream(file);
+		 
+		 workbook=new XSSFWorkbook(fis);
+		 sheet=workbook.getSheetAt(0);
+		 
+		row=sheet.getRow(rownum);
+		cell=row.getCell(cellnum);
+		
+		return(cell.getStringCellValue());
+	}
+
+	
+}
